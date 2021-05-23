cbb5241fc40185a8be65e5ef65347d44780099d1
jcrm19193 <joshua.rosas@samsung.com>
2021-05-23 12:42:54 -0500

    changes

--- ejemplos/sesion4/app.js
+++ ejemplos/sesion4/app.js
@@ -1,6 +1,8 @@
 const Axios = require ('axios');
+const Uuid = require ('node-uuid')
+let cn = null
 exports.handler = async (event) => {
-    const r = await Axios.get("https://apidev.samsung.com.pe/global/register");
+    const r = await Axios.get("https://google.com");
     console.log(r)
     // TODO implement
     const response = {
